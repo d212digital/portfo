aad88cf393c325e8c2740047a3e3c147e3618ee0
David Adamson <david@212digital.co.uk>
2021-01-26 13:54:51 +0000

    added portfo work and jupyter notebok content

--- app.py
+++ app.py
@@ -15,18 +15,20 @@ def write_to_file(data):
     with open('database.txt', mode='a') as database:
         sender_name = data["sender_name"]
         email = data["email"]
+        number = data["number"]
         subject = data["subject"]
         message = data["message"]
-        file = database.write(f'\n{sender_name},{email},{subject},{message}') 
+        file = database.write(f'\n{sender_name},{email},{subject},{number},{message}') 
 
 def write_to_csv(data):
     with open('database.csv', newline='', mode='a') as database2:
         sender_name = data["sender_name"]
         email = data["email"]
+        number = data["number"]
         subject = data["subject"]
         message = data["message"]
         csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([sender_name,email,subject,message])
+        csv_writer.writerow([sender_name,email,number,subject,message])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
@@ -36,6 +38,6 @@ def submit_form():
             write_to_csv(data)
             return redirect('/thankyou.html')
         except:
-            return 'did not save to database'
+            return 'Did not save to database'
     else:
         return 'something went wrong. Try again!'
